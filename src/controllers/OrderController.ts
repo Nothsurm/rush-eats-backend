feeeadcd4affc9ad8cb43da03dc768f2d76b3e4d
Mike Rushton <mrush94ton@gmail.com>
2024-04-04 10:23:46 +0100

    get customer orders ap

--- src/controllers/OrderController.ts
+++ src/controllers/OrderController.ts
@@ -7,6 +7,20 @@ const STRIPE = new Stripe(process.env.STRIPE_API_KEY as string);
 const FRONTEND_URL = process.env.FRONTEND_URL as string;
 const STRIPE_ENDPOINT_SECRET = process.env.STRIPE_WEBHOOK_SECRET as string;
 
+const getMyorders = async (req: Request, res: Response) => {
+    try {
+        const orders = await Order.find({ user: req.userId })
+            .populate("restaurant")
+            .populate("user")
+
+        res.json(orders)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Something went wrong" })
+        
+    }
+}
+
 type CheckoutSessionRequest = {
     cartItems: {
         menuItemId: string;
@@ -155,5 +169,6 @@ const createSession = async (
 
 export default {
     createCheckoutSession,
-    stripeWebhookHandler
+    stripeWebhookHandler,
+    getMyorders
 }
